69b567d0905bace1e193efbb77af88b468eb47d5
DOO0S <144857297+mrdoos@users.noreply.github.com>
2023-10-25 18:25:04 -0300

    Add files via upload

--- types.ts
+++ types.ts
@@ -1,1 +1,12 @@
-export type EthHDMode = 'legacy' | 'ledgerlive' | 'metamask'
+import { Address, Chain } from '@xchainjs/xchain-util'
+
+import { EthHDMode } from '../ethereum/types'
+
+export type WalletType = 'keystore' | 'ledger'
+
+export type WalletBalanceType = 'all' | 'confirmed'
+
+export type HDMode = 'default' | EthHDMode
+
+export type WalletAddress = { address: Address; type: WalletType; chain: Chain; walletIndex: number; hdMode: HDMode }
+export type WalletAddresses = WalletAddress[]
