5df8aab17735de1a889309c77a137055214554d1
DOO0S <144857297+mrdoos@users.noreply.github.com>
2023-10-25 18:24:43 -0300

    Add files via upload

--- types.ts
+++ types.ts
@@ -1,4 +1,1 @@
-import * as RD from '@devexperts/remote-data-ts'
-import { BaseAmount } from '@xchainjs/xchain-util'
-
-export type BaseAmountRD = RD.RemoteData<Error, BaseAmount>
+export type EthHDMode = 'legacy' | 'ledgerlive' | 'metamask'
