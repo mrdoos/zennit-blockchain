a9f4abad812d56a004212fcdada573b49cd8e27b
DOO0S <144857297+mrdoos@users.noreply.github.com>
2023-10-25 18:20:59 -0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,11 +1,36 @@
-import { ClientFunction } from 'testcafe'
+import { EditMenuMessages, HelpMenuMessages, ViewMenuMessages, AppMenuMessages } from '../types'
 
-export const pageUrl = '../build/index.html'
-
-export const getPageUrl = ClientFunction(() => window.location.href)
-export const getPageTitle = ClientFunction(() => document.title)
-
-export const assertNoConsoleErrors = async (t: TestController) => {
-  const { error } = await t.getBrowserConsoleMessages()
-  await t.expect(error).eql([])
+const appMenu: AppMenuMessages = {
+  'menu.app.about': 'Über {name}',
+  'menu.app.hideApp': '{name} ausblenden',
+  'menu.app.hideOthers': 'Andere ausblenden',
+  'menu.app.unhide': 'Zeige alle',
+  'menu.app.quit': '{name} beenden'
 }
+
+const editMenu: EditMenuMessages = {
+  'menu.edit.title': 'Bearbeiten',
+  'menu.edit.undo': 'Rückgängig',
+  'menu.edit.redo': 'Wiederherstellen',
+  'menu.edit.cut': 'Ausschneiden',
+  'menu.edit.copy': 'Kopieren',
+  'menu.edit.paste': 'Einfügen',
+  'menu.edit.selectAll': 'Alles auswählen'
+}
+
+const helpMenu: HelpMenuMessages = {
+  'menu.help.title': 'Hilfe',
+  'menu.help.learn': 'Mehr erfahren…',
+  'menu.help.docs': 'Dokumentation',
+  'menu.help.discord': 'Discord',
+  'menu.help.issues': 'Probleme melden'
+}
+
+const viewMenu: ViewMenuMessages = {
+  'menu.view.title': 'Ansicht',
+  'menu.view.reload': 'Neuladen',
+  'menu.view.toggleFullscreen': 'Vollbild ein/aus',
+  'menu.view.toggleDevTools': 'Dev Tools ein/aus'
+}
+
+export default { ...appMenu, ...editMenu, ...viewMenu, ...helpMenu }
