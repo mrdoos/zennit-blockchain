ecbaba65ffcde28c9bab4b9c2f3ab1043e471925
DOO0S <144857297+mrdoos@users.noreply.github.com>
2023-10-25 18:04:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+// Main entry point for Storybook@7.x
+// Based on `https://github.com/storybookjs/storybook/blob/v7.0.0-alpha.13/examples/cra-ts-essentials/.storybook/main.ts
+
+const path = require('path')
+
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
+
+const { version } = require('../package')
+
+const webpack = require('webpack')
+
+const config = {
+  features: {
+    buildStoriesJson: true,
+    breakingChangesV7: true
+  },
+  core: {
+    builder: '@storybook/builder-webpack5',
+    channelOptions: { allowFunction: false, maxDepth: 10 },
+    disableTelemetry: true
+  },
+  staticDirs: ['../public'],
+  stories: ['../src/renderer/**/*.stories.@(ts|tsx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        viewport: false,
+        docs: false
+      }
+    }
+  ],
+  framework: '@storybook/react-webpack5',
+  // Extending Storybook’s Webpack config
+  // https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+  webpackFinal: async (webpackConfig) => {
+    webpackConfig.resolve.fallback = {
+      ...webpackConfig.resolve.fallback,
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      path: require.resolve('path-browserify'),
+      fs: require.resolve('browserify-fs'),
+      assert: require.resolve('assert')
+    }
+
+    webpackConfig.plugins = [
+      ...webpackConfig.plugins,
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer']
+      }),
+      new webpack.DefinePlugin({
+        $COMMIT_HASH: JSON.stringify(new GitRevisionPlugin().commithash()),
+        $VERSION: JSON.stringify(version),
+        $IS_DEV: JSON.stringify(process.env.NODE_ENV !== 'production')
+      })
+    ]
+
+    webpackConfig.module.rules = [
+      ...webpackConfig.module.rules,
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('tailwindcss'), require('autoprefixer')]
+              }
+            }
+          }
+        ],
+        include: path.resolve(__dirname, '../')
+      }
+    ]
+
+    return webpackConfig
+  }
+}
+
+module.exports = config
---
+++ preview.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import {
+  MemoryRouter, Route, Routes } from 'react-router-dom'
+
+import { AppProvider } from '../src/renderer/contexts/AppContext'
+import { ThemeProvider, themes } from '../src/renderer/contexts/ThemeContext'
+import * as Styled from '../src/renderer/views/app/AppView.styles'
+import { Locale } from '../src/shared/i18n/types'
+import { IntlProvider } from 'react-intl'
+import { getMessagesByLocale } from '../src/renderer/i18n'
+
+import * as mockApi from '../src/shared/mock/api'
+import type { DecoratorFn } from '@storybook/react';
+
+import "../src/renderer/index.css"
+
+// Mock api provided by main renderer
+window.apiHDWallet = { ...mockApi.apiHDWallet }
+window.apiKeystore = { ...mockApi.apiKeystore }
+window.apiLang = { ...mockApi.apiLang }
+window.apiUrl = { ...mockApi.apiUrl }
+
+const lightTheme = { name: 'Light', ...themes.light }
+const _darkTheme = { name: 'Dark', ...themes.dark }
+const locale = Locale.EN
+const messages = getMessagesByLocale(locale)
+
+const providerDecorator: DecoratorFn = (Story) => (
+  <AppProvider>
+    {/* We use IntlProvider instead of our our custom I18nProvider to provide messages, but w/o dependencies to Electron/Node source, which can't run in storybook */}
+    <IntlProvider locale={locale} messages={messages} defaultLocale={locale}>
+      <ThemeProvider theme={lightTheme}>
+        <Styled.AppWrapper><Story/></Styled.AppWrapper>
+      </ThemeProvider>
+    </IntlProvider>
+  </AppProvider>
+)
+
+// Creates a `Router` decorator
+// based on https://divotion.com/blog/typescript-react-router-v6-inside-storybook-stories
+const reactRouterDecorator: DecoratorFn = (Story) => {
+  return (
+    <MemoryRouter>
+      <Routes>
+        <Route path="/*" element={<Story />} />
+      </Routes>
+    </MemoryRouter>
+  );
+};
+
+
+export const decorators = [providerDecorator, reactRouterDecorator];
+
+export const parameters = {
+  viewport: {
+    viewports: INITIAL_VIEWPORTS
+  }
+};
