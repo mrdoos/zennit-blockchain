b142c1ab3ec3b4056ab22b95d4bb1cee59664199
DOO0S <144857297+mrdoos@users.noreply.github.com>
2023-10-25 10:27:35 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const basex = require('base-x')
-const ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
-module.exports = basex(ALPHABET)
+var me = module.exports
+
+me.VERSION = require('../package.json').version
+
+var Crypto = {
+  sha256: require('sha256'),
+  ripemd160: require('ripemd160')
+}
+
+me.Address = require('btc-address')
+me.Key = require('eckey')
+me.ECDSA = require('ecdsa')
+me.BigInteger = require('bigi')
+me.Crypto = Crypto
+me.base58 = require('bs58')
+me.convertHex = require('convert-hex')
+me.secureRandom = require('secure-random')
