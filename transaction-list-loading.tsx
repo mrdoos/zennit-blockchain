c59e40ca88d4ef768437e237ca48f4a7796eea37
DOO0S <144857297+mrdoos@users.noreply.github.com>
2023-10-26 05:24:36 +0000

    commit

--- transaction-list-loading.tsx
+++
@@ -1,9 +0,0 @@
-import { templateTxBoxProps } from './transaction-list-item-pseudo';
-import { Flex, Spinner, color } from '@stacks/ui';
-import React from 'react';
-
-export const TransactionListLoading = () => (
-  <Flex {...templateTxBoxProps}>
-    <Spinner size="sm" color={color('text-caption')} />
-  </Flex>
-);
